3f3b74058fa1e0f5163bc9a2c6a2755b41927755
Sunit Parekh <parekh.sunit@gmail.com>
2012-08-02 13:57:48 +0530

    removed not required dummy initial test

--- spec/acceptance/end_2_end_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe "Investigation & Debugging Tests" do
-
-  it "test for investigating and debugging" do
-    Invoice = Utils::SourceTable.create :invoice
-    #pry.binding
-  end
-end
