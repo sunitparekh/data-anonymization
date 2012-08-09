765f2cf50e71a2313982e6d8ce456998c5847960
Satyam Agarwala <satyamag@gmail.com>
2012-08-09 19:38:08 +0530

    Random integer within given delta from original integer with spec

---
+++ lib/strategy/field/random_integer_delta.rb
@@ -0,0 +1,20 @@
+module DataAnon
+  module Strategy
+    module Field
+      class RandomIntegerDelta
+
+        DEFAULT_DELTA = 10
+
+        def initialize delta = nil
+          @delta = delta || DEFAULT_DELTA
+        end
+
+        def anonymize field
+          adjustment = rand -@delta...@delta
+          return field.value + adjustment
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/strategy/field/random_integer_delta_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe DataAnon::Strategy::Field::RandomIntegerDelta do
+
+  RandomIntegerDelta = DataAnon::Strategy::Field::RandomIntegerDelta
+  let(:field) {DataAnon::Core::Field.new('integer_field',100,1,nil)}
+
+  describe "anonymized value returned should be an integer" do
+    let(:anonymized_integer) {RandomIntegerDelta.new(10).anonymize(field)}
+
+    it { is_integer = anonymized_integer.is_a? Integer
+         is_integer.should be true
+    }
+
+  end
+
+  describe "anonymized integer should be within delta from original integer" do
+    let(:anonymized_integer) {RandomIntegerDelta.new(10).anonymize(field)}
+
+    it{anonymized_integer.should be_between(90,110)}
+  end
+
+end
