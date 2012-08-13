a93f5cd1f534fff9ca1038ae9f66b83035fc62f2
Satyam Agarwala <satyamag@gmail.com>
2012-08-13 17:42:01 +0530

    Anonymize Time with associated spec

---
+++ lib/strategy/field/anonymize_time.rb
@@ -0,0 +1,59 @@
+module DataAnon
+  module Strategy
+    module Field
+
+      class AnonymizeTime
+
+        DEFAULT_ANONYMIZATION = true
+
+        def self.only_month
+          self.new true, false, false, false, false
+        end
+
+        def self.only_day
+          self.new false, true, false, false, false
+        end
+
+        def self.only_hour
+          self.new false, false, true, false, false
+        end
+
+        def self.only_minute
+          self.new false, false, false, true, false
+        end
+
+        def initialize anonymize_month = DEFAULT_ANONYMIZATION, anonymize_day = DEFAULT_ANONYMIZATION, anonymize_hour = DEFAULT_ANONYMIZATION, anonymize_min = DEFAULT_ANONYMIZATION, anonymize_sec = DEFAULT_ANONYMIZATION
+
+          @anonymize_month = anonymize_month
+          @anonymize_day = anonymize_day
+          @anonymize_hour = anonymize_hour
+          @anonymize_min = anonymize_min
+          @anonymize_sec = anonymize_sec
+
+          @number_util = DataAnon::Utils::NumberUtils.new
+
+        end
+
+        def anonymize field
+
+          provided_time = field.value
+          year = provided_time.year
+          month = @anonymize_month? @number_util.generate_random_int(1,12) : provided_time.month
+          day = @anonymize_day? @number_util.generate_random_int(1,31) : provided_time.day
+          hour = @anonymize_hour? @number_util.generate_random_int(1,24) : provided_time.hour
+          min = @anonymize_min? @number_util.generate_random_int(1,60) : provided_time.min
+          sec = @anonymize_sec? @number_util.generate_random_int(1,60) : provided_time.sec
+
+          create_object(day, hour, min, month, sec, year)
+        end
+
+        private
+
+        def create_object(day, hour, min, month, sec, year)
+          Time.new(year, month, day, hour, min, sec)
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/strategy/field/anonymize_time_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe DataAnon::Strategy::Field::AnonymizeTime do
+
+  AnonymizeTime = DataAnon::Strategy::Field::AnonymizeTime
+  let(:field) { DataAnon::Core::Field.new('date', Time.new(2000,1,1,12,12,12), 1, nil) }
+
+  describe 'providing true only for month should randomize only the month field' do
+
+    let(:anonymized_time) { AnonymizeTime.new(true,false,false,false,false).anonymize(field) }
+
+    it {
+      anonymized_time.year.should be 2000
+      anonymized_time.day.should be 1
+      anonymized_time.hour.should be 12
+      anonymized_time.min.should be 12
+      anonymized_time.sec.should be 12
+
+      anonymized_time.month.should be_between(1,12)
+    }
+  end
+
+
+
+end
