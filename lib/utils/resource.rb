077a79c839ac5b23bcfb6175206b46ad31249695
Satyam Agarwala <satyamag@gmail.com>
2012-08-14 17:45:28 +0530

    Added method to return project directory path

--- lib/utils/resource.rb
+++ lib/utils/resource.rb
@@ -7,6 +7,10 @@ module DataAnon
         project_home =  File.join(File.dirname(__FILE__), '../../')
         "#{project_home}resources/#{file_name}"
       end
+
+      def self.project_home
+        File.join(File.dirname(__FILE__), '../../')
+      end
     end
 
   end
