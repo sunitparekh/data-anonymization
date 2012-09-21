f3b3f32e95457d52af2bd363289580c066f67e4f
Sunit Parekh <parekh.sunit@gmail.com>
2012-09-21 15:43:33 +0530

    fixed issue with powerbar color settings

--- lib/utils/progress_bar.rb
+++ lib/utils/progress_bar.rb
@@ -9,14 +9,18 @@ module DataAnon
         @total = total
         @table_name = table_name
         @power_bar = PowerBar.new if show_progress_env
+        apply_power_bar_settings  if show_progress_env
+      end
+
+      def apply_power_bar_settings
         @power_bar.settings.tty.finite.template.main = \
         "${<msg>} ${<bar> }\e[0m${<rate>/s} \e[33;1m${<percent>%} " +
             "\e[36;1m${<elapsed>}\e[31;1m${ ETA: <eta>}"
         @power_bar.settings.tty.finite.template.padchar = "\e[0m\u2589"
         @power_bar.settings.tty.finite.template.barchar = "\e[34;1m\u2589"
-        @power_bar.settings.tty.finite.template.exit = "\e[?25h\e[0m"  # clean up after us
+        @power_bar.settings.tty.finite.template.exit = "\e[?25h\e[0m" # clean up after us
         @power_bar.settings.tty.finite.template.close = "\e[?25h\e[0m\n" # clean up after us
-        @power_bar.settings.tty.finite.output = Proc.new{ |s| $stderr.print s }
+        @power_bar.settings.tty.finite.output = Proc.new { |s| $stderr.print s }
       end
 
       def show index
