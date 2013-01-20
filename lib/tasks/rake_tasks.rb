13b89388e1865d5f59fc130bafc408c92f54c798
Sunit Parekh <parekh.sunit@gmail.com>
2013-01-20 20:57:56 +0530

    updated travic-ci

--- lib/tasks/rake_tasks.rb
+++ lib/tasks/rake_tasks.rb
@@ -1,15 +1,15 @@
 require 'rake'
-require 'rake/tasklib'
+require 'rake/task'
 
 module DataAnonymization
   class RakeTasks
     include Rake::DSL if defined? Rake::DSL
 
     def initialize
-      desc "Task to build the clean empty destination database"
+      desc 'Task to build the clean empty destination database'
       task :empty_dest do
-        system "rm sample-data/chinook-empty.sqlite"
-        system "sqlite3 sample-data/chinook-empty.sqlite < sample-data/chinook_schema.sql"
+        system 'rm sample-data/chinook-empty.sqlite'
+        system 'sqlite3 sample-data/chinook-empty.sqlite < sample-data/chinook_schema.sql'
       end
     end
 
