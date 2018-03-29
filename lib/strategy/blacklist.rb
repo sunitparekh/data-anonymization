74dbad281aec5c2ca45e5a29ac94ee610c9e4d6f
Jason Barnabe <jason@kickbooster.me>
2018-03-29 11:29:57 -0500

    Use update_columns instead of save! for performance

--- lib/strategy/blacklist.rb
+++ lib/strategy/blacklist.rb
@@ -3,15 +3,16 @@ module DataAnon
     class Blacklist < DataAnon::Strategy::Base
 
       def process_record index, record
+        updates = {}
         @fields.each do |field, strategy|
           database_field_name = record.attributes.select { |k,v| k == field }.keys[0]
           field_value = record.attributes[database_field_name]
           unless field_value.nil? || is_primary_key?(database_field_name)
             field = DataAnon::Core::Field.new(database_field_name, field_value, index, record, @name)
-            record[database_field_name] = strategy.anonymize(field)
+            updates[database_field_name] = strategy.anonymize(field)
           end
         end
-        record.save!
+        record.update_columns(updates) if updates.any?
       end
 
     end
