3d15c004ec7275ae805adaae16b1823c5ac85b56
Tony Day <tony.day@reinteractive.net>
2018-11-22 18:19:35 +1300

    When finding/creating AR classes ignore inherited constants

--- spec/utils/database_spec.rb
+++ spec/utils/database_spec.rb
@@ -16,6 +16,15 @@ describe 'Utils' do
     album.all.length > 0
   end
 
+  # This is a specific example encountered in the wild
+  # Instead of creating a new class it references
+  # ActiveSupport::Callbacks::Conditionals
+  it 'creates a table for an in-scope constant' do
+    conditionals = DataAnon::Utils::SourceTable.create 'Conditionals'
+    conditionals.count.should == 0
+    conditionals.all.length == 0
+  end
+
   it 'should test the connection to destination database' do
     album = DataAnon::Utils::DestinationTable.create 'Album', ['AlbumId']
     album.count.should == 0
