5e0a5ac116ab12603e9191327d1e901c911c63d0
Nicolai Bach Woller <woller@traels.it>
2025-01-28 13:09:23 +0100

    Fix. Lease connection from AR connection pool.

--- lib/thor/helpers/rdbms_dsl_generator.rb
+++ lib/thor/helpers/rdbms_dsl_generator.rb
@@ -1,36 +1,31 @@
-require 'thor'
-require 'active_record'
-require 'erb'
+require "thor"
+require "active_record"
+require "erb"
 
 module DataAnon
   module ThorHelpers
     class RDBMSDSLGenerator
-
       def self.source_root
         File.dirname(__FILE__)
       end
 
       def generate_whitelist_script(configuration_hash)
-
         @configuration_hash = configuration_hash
-        @ar_object = ActiveRecord::Base.establish_connection(@configuration_hash)
+        connection_pool = ActiveRecord::Base.establish_connection(@configuration_hash)
+        @connection = connection_pool.lease_connection
 
-        @tables = @ar_object.connection.tables
+        @tables = @connection.tables
 
-        erb = ERB.new( File.new(RDBMSDSLGenerator.source_root + "/../templates/whitelist_template.erb").read, nil, '-')
+        erb = ERB.new(File.new(RDBMSDSLGenerator.source_root + "/../templates/whitelist_template.erb").read, trim_mode: "-")
 
-        File.open('rdbms_whitelist_generated.rb', 'w') do |f|
+        File.open("rdbms_whitelist_generated.rb", "w") do |f|
           f.write erb.result(binding)
           f.close
         end
-
       rescue => e
         puts "\e[31mActiverecord was unable to establish a connection to the specified database. Please check the configuration options and try again.\e[0m"
         puts e.backtrace
       end
-
     end
   end
 end
-
-
